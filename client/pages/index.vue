0770a8352892627b68e5b0e6f63ee9d88291865d
Joshua Yang <yss901025@gmail.com>
2018-02-22 15:50:15 +0800

    u

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,32 +1,11 @@
 <template>
   <section class="container">
-    <h2>todos count: {{list.length}}</h2>
-    <button @click="add({id:1,text:2})">add todo item</button>
-    <hr>
-    <nuxt-link to="/about">goto about</nuxt-link>
+
   </section>
 </template>
 
 <script>
-import {
-  mapState,
-  mapMutations,
-} from 'vuex';
-
 export default {
-  async asyncData (context) {
-
-  },
-  computed: {
-    ...mapState('todos', [
-      'list',
-    ]),
-  },
-  methods: {
-    ...mapMutations('todos', [
-      'add',
-    ]),
-  },
   components: {
   },
 };
